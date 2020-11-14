c33eb761031074d7a25bfa890d19a9aa2c7e8fe3
Carlos Ramírez <calbertora@gmail.com>
2020-11-14 12:01:54 -0500

    Add the button and some css

--- js/index.js
+++ js/index.js
@@ -1,1 +1,14 @@
-console.log(`Hello world!`);
+let sequence = new Array(10)
+  .fill(0)
+  .map(() => getRandomMax());
+
+function getRandomMax() {
+  const COLORS_QUANTITY = 4;
+  return Math.floor( Math.random() * COLORS_QUANTITY);
+}
+
+console.log(sequence);
+
+function setClickEvent(event) {
+  console.log(event);
+}
